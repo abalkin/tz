6e7c34cbdb0c9f6d594ba9365373092ec5b030f1
Alexander Belopolsky <a@enlnt.com>
2016-08-05 13:08:43 -0400

    Bump version.

--- tz/metadata.py
+++ tz/metadata.py
@@ -8,7 +8,7 @@ Information describing the project.
 package = 'tz'
 project = "TZ"
 project_no_spaces = project.replace(' ', '')
-version = '0.2.1'
+version = '0.2.2'
 description = 'PEP 495 compliant tzinfo implementation.'
 authors = ['Alexander Belopolsky', ]
 authors_string = ', '.join(authors)
