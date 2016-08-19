214f5f3cb7d88d0b81ed9a213fc6b3e7476ccc0d
Alexander Belopolsky <a@enlnt.com>
2016-08-19 18:05:21 -0400

    First try for guessing save from tzfiles.

--- tz/system_tzdata.py
+++ tz/system_tzdata.py
@@ -1,6 +1,8 @@
 import os
 from datetime import timedelta, datetime
 
+from tz.tools import pairs
+
 from . import tzfile
 from .tzfile import is_tzfile
 
@@ -64,6 +66,41 @@ UNIX_EPOCH = datetime(1970, 1, 1)
 CREATION = (datetime(1, 1, 1) - UNIX_EPOCH) // SECOND
 
 
+def guess_saves(zone, data):
+    """Return types with guessed DST saves"""
+    saves = {}
+    details = {}
+    for (time0, type0), (time1, type1) in pairs(data.times):
+        is_dst0 = bool(data.types[type0][1])
+        is_dst1 = bool(data.types[type1][1])
+        if (is_dst0, is_dst1) == (False, True):
+            shift = data.types[type1][0] - data.types[type0][0]
+            saves.setdefault(type1, set()).add(shift)
+            details[type1, shift] = (time0, time1)
+        elif (is_dst0, is_dst1) == (True, False):
+            shift = data.types[type1][0] - data.types[type0][0]
+            saves.setdefault(type1, set()).add(shift)
+            details[type1, shift] = (time0, time1)
+
+    types = data.types[:]
+    for i, (offset, save, abbr) in enumerate(data.types):
+        if save:
+            guesses = saves.get(i, set())
+            if not guesses:
+                guess = timedelta(hours=1)
+            elif len(guesses) == 1:
+                guess = guesses.pop()
+            else:
+                print("Multiple save value guesses for type %d in zone %s." %
+                      (i, zone))
+                for g in guesses:
+                    d = details[i, g]
+                    print("   ", g, *d)
+                guess = min(g for g in guesses if g)
+            types[i] = (offset, guess, abbr)
+    return types
+
+
 def get(zone):
     path = os.path.join(zoneinfo_dir, zone)
     with open(path, 'br') as f:
@@ -76,5 +113,6 @@ def get(zone):
     data.times = [
         (UNIX_EPOCH + timedelta(seconds=max(CREATION, t)), i)
         for t, i in zip(raw_data.times, raw_data.type_indices)]
+    data.types = guess_saves(zone, data)
     data.rules = raw_data.posix_string
     return data
