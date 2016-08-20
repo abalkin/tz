4da9a34856131c8f9fac0bedb52f9ac972fd3621
Alexander Belopolsky <a@enlnt.com>
2016-08-20 12:54:29 -0400

    Bumped versions.

--- tz/metadata.py
+++ tz/metadata.py
@@ -8,11 +8,11 @@ Information describing the project.
 package = 'tz'
 project = "TZ"
 project_no_spaces = project.replace(' ', '')
-version = '0.2.2'
+version = '1.0a1'
 description = 'PEP 495 compliant tzinfo implementation.'
 authors = ['Alexander Belopolsky', ]
 authors_string = ', '.join(authors)
 emails = ['alexander.belopolsky@gmail.com', ]
 license = 'MIT'
 copyright = '2016 ' + authors_string
-url = 'http://example.com/'
+url = 'https://abalkin.github.io/tz'
--- tzdata-pkg/setup.py
+++ tzdata-pkg/setup.py
@@ -5,6 +5,7 @@ import re
 
 from setuptools import setup
 
+PRE_RELEASE = 'a1'
 
 if isfile("MANIFEST"):
     os.unlink("MANIFEST")
@@ -19,12 +20,13 @@ with open(TOPDIR + '/raw/Makefile') as f:
             TZVERSION = m.group(1)
             break
 VERSION = "1.%s.%d" % (TZVERSION[:-1], ord(TZVERSION[-1]) - ord('a'))
+VERSION += PRE_RELEASE
 with open(TOPDIR + '/tzdata/zones') as f:
     ZONES = [line.strip() for line in f]
 
 setup(name="tzdata",
       version=VERSION,
-      description="Extensions to the standard Python datetime module",
+      description="Timezone data",
       author="Alexander Belopolsky",
       author_email="alexander.belopolsky@gmail.com",
       url="https://abalkin.github.io/tz",
@@ -34,9 +36,9 @@ The tzdata package provides the timezone data for use with the tz package.
 """,
       packages=["tzdata"],
       package_data={"tzdata": ["zones"] + ZONES},
-      zip_safe=True,
+      zip_safe=False,
       classifiers=[
-          'Development Status :: 5 - Production/Stable',
+          'Development Status :: 3 - Alpha',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: BSD License',
           'Programming Language :: Python',
